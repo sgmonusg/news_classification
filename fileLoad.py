ecfed63457851336e4128811a86d1aed3cef3025
shubhamjha97 <jha1shubham@gmail.com>
2016-08-26 15:57:19 +0530

    third

---
+++ fileLoad.py
@@ -0,0 +1,1 @@
+#fileLoad.py
--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 import numpy as np
-import pandas as pd
-import sklearn
-import os
-from tqdm import tqdm
-
+import hello
+import fileLoad
 business_loc='/home/shubham/Desktop_files/news_classification/news-sort-master/Different-news-articles-classified-master/Training set/business/'
 sport_loc='/home/shubham/Desktop_files/news_classification/news-sort-master/Different-news-articles-classified-master/Training set/sport/'
 tech_loc='/home/shubham/Desktop_files/news_classification/news-sort-master/Different-news-articles-classified-master/Training set/tech/'
 politics_loc='/home/shubham/Desktop_files/news_classification/news-sort-master/Different-news-articles-classified-master/Training set/politics/'
 entertainment_loc='/home/shubham/Desktop_files/news_classification/news-sort-master/Different-news-articles-classified-master/Training set/entertainment/'
+files=[business_loc, sport_loc, tech_loc, politics_loc, entertainment_loc]
 
-        
-print doc_dict
+fileLoad.loadFiles(files)
