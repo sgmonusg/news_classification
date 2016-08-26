b2cd0a50135b9861b50e7bfaec3c31aab1792590
shubhamjha97 <jha1shubham@gmail.com>
2016-08-26 15:31:24 +0530

    fjkjhuyg

--- main.py
+++ main.py
@@ -10,22 +10,5 @@ tech_loc='/home/shubham/Desktop_files/news_classification/news-sort-master/Diffe
 politics_loc='/home/shubham/Desktop_files/news_classification/news-sort-master/Different-news-articles-classified-master/Training set/politics/'
 entertainment_loc='/home/shubham/Desktop_files/news_classification/news-sort-master/Different-news-articles-classified-master/Training set/entertainment/'
 
-word_dict={}
-doc_dict={}
-
-for root, subdir, fil in os.walk(business_loc):
-    for x in tqdm(fil):
-        loc=business_loc+str(x)
-        buff=open(loc, 'r')
-        for line in buff:
-            words=line.lower().strip().split(' ')
-            for word in words:
-                if word not in word_dict.keys():
-                    word_dict[word]=0
-                    doc_dict[word]=[]
-                word_dict[word]=word_dict[word]+1   
-            
-                doc_dict[word].append(word_dict[word])
-    
         
 print doc_dict
