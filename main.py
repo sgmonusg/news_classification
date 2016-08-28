e0b61cd2aee296b64065f05c8a963bad725c7605
shubhamjha97 <jha1shubham@gmail.com>
2016-08-28 11:39:35 +0530

    Ad hoc prediction function built

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import fileLoad
 import newsPipeline
+import predictionFunc
 
 business_loc='/home/shubham/Desktop_files/news_classification/news-sort-master/Different-news-articles-classified-master/Training set/business/'
 sport_loc='/home/shubham/Desktop_files/news_classification/news-sort-master/Different-news-articles-classified-master/Training set/sport/'
@@ -7,9 +8,15 @@ tech_loc='/home/shubham/Desktop_files/news_classification/news-sort-master/Diffe
 politics_loc='/home/shubham/Desktop_files/news_classification/news-sort-master/Different-news-articles-classified-master/Training set/politics/'
 entertainment_loc='/home/shubham/Desktop_files/news_classification/news-sort-master/Different-news-articles-classified-master/Training set/entertainment/'
 files=[business_loc, sport_loc, tech_loc, politics_loc, entertainment_loc]
+toPredict=['/home/shubham/Desktop_files/news_classification/news-classification/forPrediction']
 
 loaded=fileLoad.loadFiles(files)
 if loaded:
     print 'Files Received!'
 
-grid_search=newsPipeline.buildPipeline(loaded)
+classifier=newsPipeline.buildPipeline(loaded)
+
+toPredict=fileLoad.loadFiles(toPredict)
+
+predictionFunc.predictionFunction(classifier, toPredict)
+#end
--- predictionFunc.py
+++ predictionFunc.py
@@ -2,5 +2,10 @@
 
 import sklearn
 
-def predictionFunction(classifier, text):
-    
+final_predictions=[]
+
+def predictionFunction(classifier, predDict):
+    print 'prediction function not yet built'
+    for x in predDict:
+        final_predictions.append(classifier.predict(x['text']))
+    print final_predictions
